af7a74e6b15db18ee6a7548f8d07110eba452d25
mabn17 <martin.d@live.se>
2021-04-05 01:54:12 +0200

    added argument to include extra assignments

--- examiner/cli_parser.py
+++ examiner/cli_parser.py
@@ -10,7 +10,7 @@ def parse():
     parser = argparse.ArgumentParser(
         formatter_class=argparse.RawTextHelpFormatter)
 
-    # what folder inside .dbwebb/test should be ran.
+    # absolute path to the test folder
     parser.add_argument(
         "-w", "--what", dest="what", required=True,
         help="REQUIRED - The absolute path to the folder containing the tests"
@@ -21,6 +21,12 @@ def parse():
         "-t", "--tags", dest="tags", default=[],
         help="Collects tags to run specific tests\n" + \
             "USAGE: -t=tag1 || -t=tag1,tag2"
+    )
+
+    # create the parser for the extra command
+    parser.add_argument(
+        "-e", "--extra", dest="extra_assignments", default=False,
+        action="store_true", help="Includes tests for extra assignments"
     )
 
     args, _empty = parser.parse_known_args()
