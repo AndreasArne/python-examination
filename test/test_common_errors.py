8f32308ee32d91255e9f9d344d6986ca440bb86e
Andreas Arnesson <andreas_osby@hotmail.com>
2021-05-18 11:19:50 +0200

    (test) Added tests to cathing assertion error when trace flag is set (refactor) Trace now show entire traceback

--- test/test_common_errors.py
+++ test/test_common_errors.py
@@ -28,6 +28,34 @@ class TestCheckIfCommonError(unittest.TestCase):
         self.assertFalse(res)
 
 class TestErrorFunctions(unittest.TestCase):
+
+
+    def test_assertion_traceback(self):
+        tb_mock = mock.MagicMock()
+        tb_mock.format.return_value = [
+            'Traceback (most recent call last):',
+            'File "/c/Users/aar/git/python-dev/.dbwebb/test/examiner/helper_functions.py", line 173, in wrapper',
+            'return f(self, *args, **kwargs)',
+            'File "/c/Users/aar/git/python-dev/.dbwebb/test/examiner/exam_test_case.py", line 87, in assertIn',
+            'super().assertIn(member, container, msg)',
+            'AssertionError: 1 != 2',
+        ]
+        with mock.patch("common_errors.ARGS.trace_assertion_error", True):
+            res = common_errors.check_if_common_error("AssertionError", tb_mock, "")
+        self.assertIn('AssertionError: 1 != 2', res)
+        self.assertTrue(isinstance(res, str))
+
+
+
+    def test_assertion_traceback_trace_false(self):
+        tb_mock = mock.MagicMock()
+
+        with mock.patch("common_errors.ARGS.trace_assertion_error", False):
+            res = common_errors.check_if_common_error("AssertionError", tb_mock, "")
+            self.assertEqual("", res)
+
+
+
     def test_wrong_nr_of_input_calls_found(self):
         tb_mock = mock.MagicMock()
         stack_obj_mock2 = mock.MagicMock()
