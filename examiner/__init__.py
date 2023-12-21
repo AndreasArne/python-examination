ef2b6479eae8aedf7a411bb4ea9f21327b55c89b
Andreas Arnesson <Andreas_osby@hotmail.com>
2023-12-21 15:09:15 +0100

    started on support for sentry

--- examiner/__init__.py
+++ examiner/__init__.py
@@ -9,3 +9,20 @@ from examiner.helper_functions import check_for_tags as tags, find_path_to_assig
 
 # Version structure major.minor[.patch][sub]
 __version__ = '2.4.0'
+
+
+import sentry_sdk
+# config https://docs.sentry.io/platforms/python/configuration/options/
+sentry_sdk.init(
+    dsn="url",
+    send_default_pii=False,
+    send_client_reports=False,
+    server_name="Jane Doe",
+    release="oopython@vt24",
+    sample_rate=1.0, # ändra denna för att inte bli för spammad?
+)
+
+# Använd tags för att specificera kmom i suite filerna? 
+# https://docs.sentry.io/platforms/python/enriching-events/tags/
+
+# läs om https://docs.sentry.io/platforms/python/enriching-events/scopes/
